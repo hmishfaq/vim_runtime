1a5a754a16971f9f162620d3966c44ed5502418c
Noah Gamboa <noah@gamboafamily.com>
2016-11-07 22:34:37 -0800

    added to dotfiles

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,14 +1,14 @@
-cd ~/.vim_runtime
+cd ~/.dotfiles
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/.dotfiles/vim/
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.dotfiles/vim/vimrcs/basic.vim
+source ~/.dotfiles/vim/vimrcs/filetypes.vim
+source ~/.dotfiles/vim/vimrcs/plugins_config.vim
+source ~/.dotfiles/vim/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/.dotfiles/vim/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
