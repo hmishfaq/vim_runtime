90ee6bd57f1bb67b37ab1ddee908ea3ce6dba77d
Noah Gamboa <noahgamboa@Noahs-MacBook-Pro.local>
2017-06-29 11:30:58 -0700

    updated basic version

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -1,3 +1,4 @@
 cd ~/.vim_runtime
 cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+cat ~/.vim_runtime/vimrcs/custom.vim >> ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
